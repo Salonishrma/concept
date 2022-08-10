494444d9e135c1d0142fba3da0456d091c3d00f5
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2022-08-11 00:38:34 +0530

    Update code.cpp

--- Weird algorithm/code.cpp
+++ Weird algorithm/code.cpp
@@ -1,6 +1,17 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<n<<" ";
+    while(n!=1){
+        if(n%2==0){
+            n=n/2;
+        }
+        else{
+            n=(n*3)+1;
+        }
+        cout<<n<<" ";
+    }
+    return 0;
 }
